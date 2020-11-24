0e89fb377c490f2574419cd8ed75e5f52063e74a
Amjed Saleel K <amjedsaleel@gmail.com>
2020-11-24 20:37:34 +0530

    username eeror display in html form in register page

--- static/js/register.js
+++ static/js/register.js
@@ -1,7 +1,10 @@
 const usernameField = document.getElementById('usernameField');
-
+const feedBackFiled = document.querySelector('.invalid-feedback')
 usernameField.addEventListener('keyup', (e) => {
     const usernameValue = e.target.value;
+
+     usernameField.classList.remove("is-invalid");
+     feedBackFiled.style.display = "none";
 
     if (usernameValue.length > 0) {
         fetch("/authentication/validate-username", {
@@ -9,6 +12,11 @@ usernameField.addEventListener('keyup', (e) => {
             method: "POST",
         }).then(res => res.json()).then(data => {
             console.log('data', data)
+            if (data.username_error) {
+                usernameField.classList.add("is-invalid");
+                feedBackFiled.style.display = "block";
+                feedBackFiled.innerHTML = `<p>${data.username_error} </p>`
+            }
         })
     }
 });

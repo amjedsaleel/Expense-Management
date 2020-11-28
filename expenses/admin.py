9e9c9aa2d15b05a3335c9c893c63a86ae64ef6a4
Amjed Saleel K <amjedsaleel@gmail.com>
2020-11-28 17:12:34 +0530

    models created for expense and form designed

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,11 @@
+# django
 from django.contrib import admin
 
+# Local Django
+from . models import Expense, Category
+
 # Register your models here.
+
+admin.site.register(Expense)
+admin.site.register(Category)
+

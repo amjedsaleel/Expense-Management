a2bec9de23d48f801a4a253560c5449e378aeeaa
Amjed Saleel K <amjedsaleel@gmail.com>
2020-11-30 16:27:55 +0530

    expense update and delete option seted

--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,6 @@
 # Django
+import pkgutil
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -7,7 +9,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 
 # Local django
-from . models import Expense, Category
+from .models import Expense, Category
+
 
 # Create your views here.
 
@@ -52,3 +55,39 @@ class AddExpenseView(View):
         return redirect('expenses:expenses')
 
 
+def edit_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories
+    }
+
+    if request.method == 'GET':
+        return render(request, 'expenses/edit-expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/edit-expense.html', context)
+
+        expense.owner = request.user
+        expense.amount = amount
+        expense.date = request.POST['date']
+        expense.category = request.POST['category']
+        expense.description = request.POST['description']
+
+        expense.save()
+        messages.success(request, 'Expense is updated')
+
+        return redirect('expenses:expenses')
+
+
+def expense_delete(request, id):
+    expense = Expense.objects.get(pk=id).delete()
+    messages.success(request, 'Expense is deleted')
+    return redirect('expenses:expenses')

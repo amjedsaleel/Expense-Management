0fde1c3b77159442bb9fb228c97aaba34a0d94db
Amjed Saleel K <amjedsaleel@gmail.com>
2020-12-04 19:28:40 +0530

    bug fixed

--- expenses/urls.py
+++ expenses/urls.py
@@ -10,7 +10,7 @@ app_name = 'expenses'
 urlpatterns = [
     path('', views.index, name='expenses'),
     path('add-expenses', views.AddExpenseView.as_view(), name='add-expense'),
-    path('edit-expense/<int:pk>', views.edit_expense, name='edit-expense'),
+    path('edit-expense/<int:id>', views.edit_expense, name='edit-expense'),
     path('expense-delete/<int:id>', views.expense_delete, name='expense-delete'),
 
     # API
